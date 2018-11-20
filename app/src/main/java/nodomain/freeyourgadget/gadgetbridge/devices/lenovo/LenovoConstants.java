29280c85555d59c7882c8aa150a8811ea0580469
Nabil BENDAFI <nabil@bendafi.fr>
2018-11-20 13:22:02 +0100

    Lenovo HW01 characteristic UUID

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/lenovo/LenovoConstants.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/lenovo/LenovoConstants.java
@@ -2,6 +2,17 @@ package nodomain.freeyourgadget.gadgetbridge.devices.lenovo;
 
 import java.util.UUID;
 
+import static nodomain.freeyourgadget.gadgetbridge.service.btle.AbstractBTLEDeviceSupport.BASE_UUID;
+
 public class LenovoConstants {
-    public static final UUID UUID_SERVICE_LENOVO = UUID.fromString("14701820-620a-3973-7c78-9cfff0876abd");
+    public static final UUID UUID_SERVICE_LENOVO_HW01_A = UUID.fromString((String.format(AbstractBTLEDeviceSupport.BASE_UUID, "190A")));
+    public static final UUID UUID_SERVICE_LENOVO_HW01_A = UUID.fromString((String.format(AbstractBTLEDeviceSupport.BASE_UUID, "190B")));
+
+    public static final UUID UUID_SERVICE_GENERIC_ACCESS = UUID.fromString((String.format(AbstractBTLEDeviceSupport.BASE_UUID, "1800")));
+    public static final UUID UUID_CHARACTERISTIC_DEVICE_NAME = UUID.fromString((String.format(AbstractBTLEDeviceSupport.BASE_UUID, "2A00")));
+
+    public static final UUID UUID_CHARACTERISTIC_RX = UUID.fromString(String.format(BASE_UUID, "0003"));
+    public static final UUID UUID_CHARACTERISTIC_TX = UUID.fromString(String.format(BASE_UUID, "0004"));
+
+    public static final UUID UUID_CHARACTERISTIC_CLIENT_CONFIG = UUID.fromString((String.format(AbstractBTLEDeviceSupport.BASE_UUID, "2902")));
 }
--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/lenovo/LenovoCoordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/lenovo/LenovoCoordinator.java
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2017 Nabil Bendafi
+/*  Copyright (C) 2018 Nabil Bendafi
 
     This file is part of Gadgetbridge.
 
